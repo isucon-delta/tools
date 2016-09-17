00639d8ef9ac29b2c0269adc8a49c50268723753
Masahiro Taguchi <sinner315@gmail.com>
2016-09-17 18:40:07 +0900

    mackerelカスタム設定、ベンチマーク前ログ退避

---
+++ mackerel-custom.sh
@@ -0,0 +1,38 @@
+## 公式プラグインの導入
+sudo apt-get install mackerel-agent-plugins mackerel-check-plugins
+
+## nginxのプロセス監視とhttpアクセスステータス監視追加
+plugin_checks=$(cat << EOS
+
+[plugin.checks.check_nginx_worker]
+command = "check-procs -p nginx --user www-data"
+
+[plugin.checks.access_status]
+command = '''
+  check-log                      \
+    --file /var/log/nginx/access.log            \
+    --pattern 'HTTP/1\.[01]" [45][0-9][0-9] '   \
+    --exclude 'GET .*?robots\.txt HTTP/1\.[01]' \
+    --warning-over 3 --critical-over 10         \
+    --return
+'''
+EOS
+)
+echo $plugin_checks >> /etc/mackerel-agent/mackerel-agent.conf
+
+## mysqldの監視
+sudo mysql -u root -e \
+"create user 'monitor_user'@'127.0.0.1'; \
+grant PROCESS, REPLICATION CLIENT on *.* to 'monitor_user'@'127.0.0.1'; \
+flush privileges;"
+
+mysql_metrics=$(cat << EOS
+
+[plugin.metrics.mysql]
+command = "/usr/local/bin/mackerel-plugin-mysql -username=monitor_user"
+EOS
+)
+echo $mysql_metrics >> /etc/mackerel-agent/mackerel-agent.conf
+
+### agentの再起動
+sudo /etc/init.d/mackerel-agent restart
---
+++ prebench.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ex
+
+if [ -f /var/lib/mysql/mysqld-slow.log ]; then
+    sudo mv /var/lib/mysql/mysqld-slow.log /var/lib/mysql/mysqld-slow.log.$(date "+%Y%m%d_%H%M%S")
+fi
+if [ -f /var/log/nginx/access.log ]; then
+    sudo mv /var/log/nginx/access.log /var/log/nginx/access.log.$(date "+%Y%m%d_%H%M%S")
+fi
+sudo systemctl restart mysql
+sudo systemctl restart nginx
